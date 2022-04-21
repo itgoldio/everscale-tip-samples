7808fd4bc7a5a8b1a3babaacf23cf3cd9d29e7f4
nikitaghost <nekhd@yandex.ru>
2022-04-21 09:09:02 +0000

    TIP4_2 update @itgold/everscale-tip package version

--- demo/TIP4_2/contracts/Collection.sol
+++ demo/TIP4_2/contracts/Collection.sol
@@ -11,6 +11,12 @@ import './Nft.sol';
 
 contract Collection is TIP4_2Collection, OwnableExternal {
 
+    /**
+    * Errors
+    **/
+    uint8 constant sender_is_not_owner = 101;
+    uint8 constant value_is_less_than_required = 102;
+
     /// _remainOnNft - the number of crystals that will remain after the entire mint 
     /// process is completed on the Nft contract
     uint128 _remainOnNft = 0.3 ton;
@@ -32,7 +38,7 @@ contract Collection is TIP4_2Collection, OwnableExternal {
     function mintNft(
         string json
     ) external virtual {
-        require(msg.value > _remainOnNft + 0.1 ton, CollectionErrors.value_is_less_than_required);
+        require(msg.value > _remainOnNft + 0.1 ton, value_is_less_than_required);
         tvm.rawReserve(0, 4);
 
         uint256 id = uint256(_totalSupply);
@@ -61,9 +67,14 @@ contract Collection is TIP4_2Collection, OwnableExternal {
     
     }
 
-    function setRemainOnNft(uint128 remainOnNft) external virtual onlyOwner {
+    function setRemainOnNft(uint128 remainOnNft) external virtual {
+        require(TIP4_1Collection._isOwner(), sender_is_not_owner);
         _remainOnNft = remainOnNft;
     } 
+
+    function _isOwner() internal override onlyOwner returns(bool){
+        return true;
+    }
 
     function _buildNftState(
         TvmCell code,
