f8c82b83dad0ceee695935791a84fb579f52640f
nikitaghost <nekhd@yandex.ru>
2022-07-11 15:18:27 +0000

    use prompts in script

--- demo/NFTAuthDebot/scripts/1-deploy-debot.js
+++ demo/NFTAuthDebot/scripts/1-deploy-debot.js
@@ -1,10 +1,34 @@
+const {
+  isValidTonAddress
+} = require('./utils');
+const prompts = require('prompts');
+
 async function main() {
+
+  const promptsData = [];
+
+  promptsData.push({
+    type: 'text',
+    name: 'ownerPubkey',
+    message: 'Owner pubkey',
+    validate: value => !!value
+  });
+
+  promptsData.push({
+    type: 'text',
+    name: 'collection',
+    message: 'Collection address',
+    validate: value => isValidTonAddress(value) ? true : 'Invalid TON address'
+  });
+
+  const response = await prompts(promptsData);
+
   const Debot = await locklift.factory.getContract('NFTAuthDebot');
   const [keyPair] = await locklift.keys.getKeyPairs();
 
-  const ownerPubkey = "0x" + keyPair.public;
+  const ownerPubkey = response.ownerPubkey;
+  const collection = response.collection;
   const nftIndexHelper = "0:4a6c2af0bcf6ca365ce7d63f55690ef300d1272d717983fc39a7603a5cde4365";
-  const collection = "0:b7b7c26eb58b8e0eca0bd9c9a2ea7e44fc45a514e308cffa2f64bd325aa887dd";
   const nftList = [];
 
   const debot = await locklift.giver.deployContract({
